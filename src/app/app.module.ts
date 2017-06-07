98c33b609e9e3b1ad25db68f87e41321e7d37b68
Dip Patel <d21patel@gmail.com>
2017-06-07 14:24:22 -0400

    2.18 Cli Added Servers Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component'
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component'
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
