26780048df353569561a3ea9ed7ca1126bb330d3
Dip Patel <d21patel@gmail.com>
2017-06-08 13:38:23 -0400

    2.29 Two-Way Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -15,7 +15,9 @@ import { SuccessComponent} from './successAlert/successAlert.component';
     SuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer =false;
   serverCreationStatus = "No server was created!";
-  serverName = "";
+  serverName = "Testttt";
   constructor() { 
     setTimeout(()=>{this.allowNewServer = true},2000);
   }
