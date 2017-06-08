f2eddc81ac76c975e4c665bedfac7991c679f5c8
Dip Patel <d21patel@gmail.com>
2017-06-08 13:28:01 -0400

    2.28 event reserved word

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer =false;
   serverCreationStatus = "No server was created!";
-
+  serverName = "";
   constructor() { 
     setTimeout(()=>{this.allowNewServer = true},2000);
   }
@@ -18,5 +18,8 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = "Server was created!"
+  }
+  onUpdateServerName(event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
